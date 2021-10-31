06d85bda12a87eeb8b426791a8035e3bd9be2e49
Udhay Hariharan <udhay@madstreetden.com>
2021-10-31 11:26:11 +0530

    Print class added

---
+++ ImageUtils/src/main/java/com/example/image_utils/BlurDetection.java
@@ -0,0 +1,10 @@
+package com.example.image_utils;
+
+import android.util.Log;
+
+public class BlurDetection {
+    private static String TAG = "Blox sdk";
+    public static void d(String msg){
+        Log.d(TAG, msg);
+    }
+}
